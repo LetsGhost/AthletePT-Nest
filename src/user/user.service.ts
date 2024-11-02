790b4640fe7f7c2d9f4c013c841d1cc1aeb81b59
Paul Ehrling <ghostlest330@gmail.com>
2024-11-02 17:07:06 +0100

    Inject user model into UserService and check for existing user by email in createUser method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   async createUser(userData: any) {
     try {
+      const existingUser = await this.userModel.findOne({ email: userData.email });
     } catch (error) {
       Logger.error('Error in creating user', error);
       return {
