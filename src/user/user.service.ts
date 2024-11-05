4018b9639662599d7069614c7a68b309dcd4f50f
Paul Ehrling <ghostlest330@gmail.com>
2024-11-05 15:56:22 +0100

    Enhance user creation logic with error handling and logging for existing users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './user.schema';
 import { Model } from 'mongoose';
+import { User } from './user.schema';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  private readonly logger = new Logger(UserService.name);
   async createUser(userData: any) {
     try {
-      const existingUser = await this.userModel.findOne({ email: userData.email });
+      const existingUser = await this.userModel.findOne({
+        email: userData.email,
+      });
+
+      if (existingUser) {
+        this.logger.error('User already exists');
+        return {
+          success: false,
+          code: 400,
+          message: 'User already exists',
+        };
+      }
+
+      const user = new this.userModel({
+        email: userData.email,
+        password: userData.password,
+        role: 'user',
+      });
+      await user.save();
+
+      this.logger.log('User created successfully');
+      return {
+        success: true,
+        code: 201,
+        user: user,
+      };
     } catch (error) {
-      Logger.error('Error in creating user', error);
+      this.logger.error('Error in creating user', error);
       return {
         success: false,
         code: 500,
