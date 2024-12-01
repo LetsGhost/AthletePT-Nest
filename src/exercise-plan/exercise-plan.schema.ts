5ff77e903d142041d1cd65c5fed495ccedcb5178
Paul Ehrling <ghostlest330@gmail.com>
2024-12-01 18:23:16 +0100

    Add Exercise and ExerciseDay schemas to ExercisePlan with service method for creation

--- src/exercise-plan/exercise-plan.schema.ts
+++ src/exercise-plan/exercise-plan.schema.ts
@@ -3,11 +3,68 @@ import { HydratedDocument, Types } from 'mongoose';
 
 export type ExercisePlanDocument = HydratedDocument<ExercisePlan>;
 
+@Schema()
+export class Exercise {
+  @Prop({ required: true })
+  exercise: string;
+
+  @Prop({ required: true })
+  weight: string;
+
+  @Prop({ required: true })
+  sets: number;
+
+  @Prop({ required: true })
+  warmupSets: number;
+
+  @Prop({ required: true })
+  warmupWeight: string;
+
+  @Prop({ required: true })
+  warmupRepetitions: string;
+
+  @Prop({ required: true })
+  repetitions: string;
+
+  @Prop({ required: true })
+  rest: string;
+
+  @Prop({ required: true })
+  execution: string;
+}
+
+@Schema()
+export class ExerciseDay {
+  @Prop({ required: true })
+  dayNumber: number;
+
+  @Prop({ required: true })
+  weekDay: string;
+
+  @Prop({ required: true })
+  type: string;
+
+  @Prop({ default: false, required: true })
+  trainingDone: boolean;
+
+  @Prop({ default: false, required: true })
+  trainingMissed: boolean;
+
+  @Prop({ required: true })
+  exercises: Exercise[];
+
+  @Prop({ type: Types.ObjectId, ref: 'Warmup' })
+  warmup: { type: Types.ObjectId; ref: 'Warmup' }[];
+}
+
 @Schema({
   timestamps: true,
 })
 export class ExercisePlan {
   _id: Types.ObjectId;
+
+  @Prop({ required: true })
+  exerciseDays: ExerciseDay[];
 
   @Prop({ default: Date.now })
   createdAt: Date;
--- src/exercise-plan/exercise-plan.service.ts
+++ src/exercise-plan/exercise-plan.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ServiceResponse } from 'src/common/interfaces/service-response.interface';
+import { ExercisePlan } from './exercise-plan.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ExercisePlanService {}
+export class ExercisePlanService {
+  private readonly logger = new Logger(ExercisePlanService.name);
+
+  constructor(
+    @InjectModel(ExercisePlan.name)
+    private readonly exerciseModel: Model<ExercisePlan>,
+  ) {}
+
+  async create(): Promise<ServiceResponse<ExercisePlan>> {
+    try {
+    } catch (error) {
+      return {
+        success: false,
+        code: 500,
+        message: 'Error creating exercise plan',
+      };
+    }
+  }
+}
